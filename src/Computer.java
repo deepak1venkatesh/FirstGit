e2754cd67fa5b1067e9720b9277ab4ac794a0cb4
deepak1venkatesh <deepak1venkatesh@gmail.com>
2023-04-05 10:56:00 +0530

    FirstGit

---
+++ src/Computer.java
@@ -0,0 +1,8 @@
+
+public class Computer
+{
+public void computerModel() 
+{
+System.out.println("Computer model is a single component");
+}
+}
---
+++ src/Desktop.java
@@ -0,0 +1,16 @@
+
+public class Desktop extends Computer
+{
+private void desktopSize() 
+{
+System.out.println("Desktop consist of CPU,MONITOR,mouse,keyboard");
+}
+
+public static void main(String[] args) {
+
+Desktop d=new Desktop();
+d.desktopSize();
+d.computerModel();
+}
+
+}
